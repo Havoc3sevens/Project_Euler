051a73b6041d394767f5172c1ff38425fdcb7c57
Havoc3sevens <ehurtado9@gmail.com>
2022-02-19 13:09:47 -0800

    CPP ID 1

---
+++ CPP/ID_1/Source_Code/main.cpp
@@ -0,0 +1,8 @@
+/*
+This program produces a logistic map text file.
+    Created by: Enrique Hurtado
+    Date: 07 February 2022
+    History:
+        Date: 02/07/22 || Mod: Program written || By: Enrique Hurtado
+*/
+#include <cstdio>
