1fbe5bbe6a670b466c7c589899482d42706eef01
Havoc3sevens <ehurtado9@gmail.com>
2022-02-20 21:19:16 -0800

    ID 1 solved

--- CPP/ID_1/Source_Code/main.cpp
+++ CPP/ID_1/Source_Code/main.cpp
@@ -1,8 +1,22 @@
 /*
-This program produces a logistic map text file.
+This program solves Project Euler ID 1
     Created by: Enrique Hurtado
-    Date: 07 February 2022
+    Date: 20 February 2022
     History:
-        Date: 02/07/22 || Mod: Program written || By: Enrique Hurtado
+        Date: 02/20/22 || Mod: Program written || By: Enrique Hurtado
 */
 #include <cstdio>
+
+int main(void) {
+    int n;
+    int sum;
+
+    n = 10000;
+    sum = 0;
+    for (int i = 1; i < n; ++i) {
+        if (i%3 == 0 || (i%5 == 0)) {
+            sum += i;
+        }
+    }
+    printf("\nTotal = %d\n\n",sum);
+}
