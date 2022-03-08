9c239c067ebcdcead24c3c7c4b93e6b39251589c
Havoc3sevens <ehurtado9@gmail.com>
2022-03-07 19:50:44 -0800

    Init ID_4

---
+++ CPP/ID_4/Source_Code/main.cpp
@@ -0,0 +1,11 @@
+/*
+This program solves Project Euler ID 4
+    Created by: Enrique Hurtado
+    Date: 07 March 2022
+    History:
+        Date: 03/07/22 || Mod: Program written || By: Enrique Hurtado
+*/
+#include <cstdio>
+
+int main(void) {
+}
