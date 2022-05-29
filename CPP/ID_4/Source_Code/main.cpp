b6ca1eaaaab8bbda681a4b12b45c3934cfba0381
Havoc3sevens <ehurtado9@gmail.com>
2022-05-28 17:42:02 -0700

    problem 4 solved

--- CPP/ID_4/Source_Code/main.cpp
+++ CPP/ID_4/Source_Code/main.cpp
@@ -4,9 +4,49 @@ This program solves Project Euler ID 4
     Date: 07 March 2022
     History:
         Date: 03/07/22 || Mod: Program written || By: Enrique Hurtado
+        Date: 05/28/22 || Mod: Program finished || By: Enrique Hurtado
 */
-#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string revers_string(string s_in) {
+    string s_out;
+    for (int i=s_in.size()-1; i >= 0; --i) {
+        s_out.push_back(s_in[i]);
+    }
+    return s_out;
+}
+
+bool check_palin(int int_in) {
+    string s_1;
+    string s_2;
+    bool palin = true;
+    s_1 = to_string(int_in);
+    s_2 = revers_string(s_1);
+    if (s_1 != s_2) {
+        palin = false;
+    }
+    return palin;
+}
 
 int main(void) {
-    printf("Test!\n");
+    int n = 999;
+    int max_num = 0;
+
+    for (int i=1; i <=n; ++i) {
+        for (int j=i; j <= n; ++j) {
+            if (check_palin(i*j)) {
+                if ((i*j)>max_num) {
+                    max_num = i*j;
+                } 
+            }
+        }
+    }
+
+    cout << max_num << endl;
+
+    return 0;
+
 }
