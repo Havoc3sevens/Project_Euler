d8f2ff04ce98a1fb83b94749e3061c0099bebeaa
Havoc3sevens <ehurtado9@gmail.com>
2022-08-19 18:35:56 -0700

    ID_9 update

--- CPP/ID_9/Source_Code/main.cpp
+++ CPP/ID_9/Source_Code/main.cpp
@@ -8,36 +8,28 @@ This program solves Project Euler ID 8
 #include <cstdio>
 
 int main(void) {
-    int n;
-    int sq_sm;
-    int sum_all;
+    int res;
+    int r_i;
+    int r_j;
+    int r_k;
     bool test = true;
-    n = 20;
-    int i = 1;
-    sum_all = 1;
-    do{
+
+    for (int i = 1; test; ++i) {
         for (int j = i-1; j >0; --j) {
             for (int k = j-1; k >0; --k) {
-                sq_sm = k*k + j*j;
-                if (sq_sm == i*i) {
-                    sum_all = k+j+i;
-                    printf("%d", sum_all != 1000);
-                    if (sum_all == 1000) {
-                        printf("i = %d, j = %d, k = %d\n", i, j, k);
-                        printf("j*j = %d, k*k = %d, k*k + j*j = %d\n", j*j, k*k, sq_sm);
-                        printf("sum_all = %d\n", sum_all);
+                if (k*k + j*j == i*i) {
+                    if (k+j+i == 1000) {
+                        res = k*j*i;
+                        r_i = i;
+                        r_j = j;
+                        r_k = k;
                         test = false;
                     }
-                    printf("sum_all = %d\n", sum_all);
                 }
-                printf("sum_all = %d\n", sum_all);
             }
-            printf("sum_all = %d\n", sum_all);
         }
-        printf("sum_all = %d\n", sum_all);
-        ++i;
-    } while (test);
-
-    printf("sum_all = %d\n", sum_all);
+    }
     
+    printf("j = %d, k = %d, i = %d\n", r_j, r_k, r_i);
+    printf("res = %d\n", res);
 }
