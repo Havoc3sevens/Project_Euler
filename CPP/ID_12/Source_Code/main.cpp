a7f6f2ee13c992d0839960ff5e9f6c06ef35f549
Havoc3sevens <ehurtado9@gmail.com>
2022-09-04 21:41:29 -0700

    Init problem 12

---
+++ CPP/ID_12/Source_Code/main.cpp
@@ -0,0 +1,16 @@
+/*
+This program solves Project Euler ID 12
+    Created by: Enrique Hurtado
+    Date: 04 September 2022
+    History:
+        Date: 09/04/22 || Mod: Program written || By: Enrique Hurtado
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+    return 0;
+
+}
