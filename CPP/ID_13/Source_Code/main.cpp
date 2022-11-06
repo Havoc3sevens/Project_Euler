ff5717805a326a8b641da346e199fc1c77e34a97
Havoc3sevens <85142373+Havoc3sevens@users.noreply.github.com>
2022-11-06 00:01:00 -0700

    ID_13 brute force

--- CPP/ID_13/Source_Code/main.cpp
+++ CPP/ID_13/Source_Code/main.cpp
@@ -15,11 +15,17 @@ using namespace std;
 int main(void) {
     string line;
     vector<string> line_read;
+    int long sum_int;
+    int long res;
     ifstream data_in("alt_data.txt");
 
+    sum_int;
     while(getline(data_in,line)) {
         line_read.push_back(line);
+        sum_int += stol(line);
     }
 
-    cout << atol(line_read[0]) << endl;
+    res = stol(to_string(sum_int).substr(0,10));
+
+    cout << res << endl;
 }
