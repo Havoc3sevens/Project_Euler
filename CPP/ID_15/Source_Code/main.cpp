32bcd387d0f13a88b7975e72a19c0a68c4439e71
Havoc3sevens <85142373+Havoc3sevens@users.noreply.github.com>
2022-11-14 22:16:01 -0800

    Init ID 15

---
+++ CPP/ID_15/Source_Code/main.cpp
@@ -0,0 +1,13 @@
+/*
+This program solves Project Euler ID 15
+    Created by: Enrique Hurtado
+    Date: 14 November 2022
+    History:
+        Date: 11/14/22 || Mod: Program written || By: Enrique Hurtado
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+}
