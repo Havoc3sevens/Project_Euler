797b29f0e1f224ffe9489c259de321fb2bbe2407
Havoc3sevens <ehurtado9@gmail.com>
2023-01-09 00:14:03 -0800

    brute force

--- CPP/ID_15/Source_Code/main.cpp
+++ CPP/ID_15/Source_Code/main.cpp
@@ -6,8 +6,36 @@ This program solves Project Euler ID 15
         Date: 11/14/22 || Mod: Program written || By: Enrique Hurtado
 */
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(void) {
+    vector <pair<int,int>> vec_init={};
+    vector <pair<int,int>> vec={};
+    int nn = 5;
+    int count = 0;
+    int ii = 0;
+    vec_init.push_back(make_pair(0,0));
+    while(vec_init.size() != 0) {
+        vec = {};
+        for(auto i = 0; i != vec_init.size(); ++i) {
+            if ((vec_init[i].first + 1) != nn+1) {
+                vec.push_back(make_pair((vec_init[i].first + 1),vec_init[i].second));
+            }
+            if ((vec_init[i].second + 1) != nn+1) {
+                vec.push_back(make_pair(vec_init[i].first,(vec_init[i].second + 1)));
+            }
+            if ((vec_init[i].first == nn) && (vec_init[i].second == nn)) {
+                count += 1;
+            }
+        }
+        vec_init = vec;
+        cout << ii << endl;
+        cout << "---" << endl;
+        ii += 1;
+    }
+
+    cout << count << endl;
+
 }
