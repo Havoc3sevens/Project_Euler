360b42fec5786ac64957ab2f6e0176d50b3e9631
Havoc3sevens <ehurtado9@gmail.com>
2023-01-27 18:10:51 -0800

    ID 15 solved

--- CPP/ID_15/Source_Code/main.cpp
+++ CPP/ID_15/Source_Code/main.cpp
@@ -4,66 +4,27 @@ This program solves Project Euler ID 15
     Date: 14 November 2022
     History:
         Date: 11/14/22 || Mod: Program written || By: Enrique Hurtado
+        Date: 01/27/22 || Mod: Pascal Triagle || By: Enrique Hurtado
 */
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-void matrix_nearest_nbr(int n, vector <pair<int,int>> &vec_out) {
-    int nn = (n+1)*(n+1);
-    int ii;
-    int jj;
-    int mtrx[nn][nn];
-    int count=1;
-
-    for(int i=0; i<=n; ++i) {
-        for(int j=0; j<=n; ++j) {
-            mtrx[i][j] = count;
-            count ++;
-        }
+double f(int n) {
+    double res=1;
+    for(int i=n; i>0; i--){
+        res = res*i;
     }
-
-    for(int i=0; i<=n; ++i) {
-        for(int j=0; j<=n; ++j) {
-            ii = i + 1;
-            jj = j + 1;
-            if (ii != n+1) {
-                vec_out[mtrx[i][j]].first = mtrx[ii][j];
-            }
-            if (jj != n+1) {
-                vec_out[mtrx[i][j]].second = mtrx[i][jj];
-            }
-         }
-    }
+    return res;
 }
 
 int main(void) {
-    vector <pair<int,int>> vec_init={};
-    vector <pair<int,int>> vec={};
-    int nn = 5;
-    int count = 0;
-    int ii = 0;
-    vec_init.push_back(make_pair(0,0));
-    while(vec_init.size() != 0) {
-        vec = {};
-        for(auto i = 0; i != vec_init.size(); ++i) {
-            if ((vec_init[i].first + 1) != nn+1) {
-                vec.push_back(make_pair((vec_init[i].first + 1),vec_init[i].second));
-            }
-            if ((vec_init[i].second + 1) != nn+1) {
-                vec.push_back(make_pair(vec_init[i].first,(vec_init[i].second + 1)));
-            }
-            if ((vec_init[i].first == nn) && (vec_init[i].second == nn)) {
-                count += 1;
-            }
-        }
-        vec_init = vec;
-        cout << ii << endl;
-        cout << "---" << endl;
-        ii += 1;
-    }
+    //after realizing that a programming method was not the most practical
+    //I noticed a pattern with the number of path per row and column
+    //it was related to pascal's triangle. I used that relation to
+    //come up with an answer. My previous attempts are saved on GitHub
+    int k = 20;
+    int n = (2*k);
 
-    cout << count << endl;
-
+    cout << long (f(n)/(f(k)*f(n-k))) << endl;
 }
