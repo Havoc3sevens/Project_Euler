6547d432be822e771f90c9fd4faefb91b1266c93
Havoc3sevens <ehurtado9@gmail.com>
2023-03-10 19:26:10 -0800

    ID 19 complete

--- CPP/ID_19/Source_Code/main.cpp
+++ CPP/ID_19/Source_Code/main.cpp
@@ -5,26 +5,6 @@ This program solves Project Euler ID 19
     History:
         Date: 03/05/23 || Mod: Program written || By: Enrique Hurtado
 */
-
-/*
-You are given the following information, but you may prefer to do some research for yourself.
-
-1 Jan 1900 was a Monday.
-Thirty days has September,
-April, June and November.
-All the rest have thirty-one,
-Saving February alone,
-Which has twenty-eight, rain or shine.
-And on leap years, twenty-nine.
-A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
-How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
-*/
-
-/*
-itterate by day. Process the day from 0. Use rules to determin the day to return.
-nope. Just calculate total days and iterate.
-*/
-
 #include <iostream>
 
 using namespace std;
@@ -33,14 +13,48 @@ int day_num_of_week(int day_in) {
     return day_in%7;
 }
 
-int main (void) {
-    int n = 100;
-    int sundays;
-    sundays = 0;
-    for (int i = 1; i <= n; i++) {
-        if (not(day_num_of_week(i))) {
-            sundays += 1;
+int days_in_year(int year_in) {
+    int days;
+    days = 30*4; //sep, apr, jun, nov
+    days += 28; //feb
+    days += 31*7; //rest
+    if(not(year_in%4)) {
+        if(not(year_in%100)) {
+            if(not(year_in%400)) {
+                ;
+            } else {
+                return days;
+            }
         }
+        days += 1;
     }
+    return days;
+}
+
+int main (void) {
+    int sundays = 0;
+    int days;
+    int start_year;
+    int end_year;
+    int start_day;
+
+    start_year = 1900;
+    start_day = 0;
+    end_year = 2000;
+    days = start_day;
+
+    for (int i = start_year; i <= end_year; i++) {
+        days += days_in_year(i);
+        if(i >= 1901) {
+            for (int d = start_day; d <= days; d++) {
+                if(day_num_of_week(d)==6) {
+                    sundays += 1;
+                }
+            }
+        }
+        start_day = days+1;
+    }
+
     cout << sundays << endl;
+
 }
