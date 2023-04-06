e3a729f73229287f291f25f52c3a0ef4556c88e2
Havoc3sevens <ehurtado9@gmail.com>
2023-04-06 13:03:28 -0700

    Init ID_20

---
+++ CPP/ID_20/Source_Code/main.cpp
@@ -0,0 +1,25 @@
+/*
+This program solves Project Euler ID 20
+    Created by: Enrique Hurtado
+    Date: 15 March 2023
+    History:
+        Date: 03/15/23 || Mod: Program written || By: Enrique Hurtado
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void array_test(int x, int arr_in[], int a_size) {
+    arr_in[0] = 1*x;
+}
+
+int main (void) {
+    int v1 = 7;
+    int v2 = 7158;
+    int arr[1];
+    array_test(x,arr,1);
+    cout << arr[0] << endl;
+    array_test(x,arr,1);
+    cout << arr[0] << endl;
+}
