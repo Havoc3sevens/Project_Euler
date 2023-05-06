bb0652b26e2a7fb794ced5d501ec3eefe2a54dcf
Havoc3sevens <ehurtado9@gmail.com>
2023-05-05 18:37:09 -0700

    large product done

--- CPP/ID_20/Source_Code/a_prod.cpp
+++ CPP/ID_20/Source_Code/a_prod.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 void times_engine(int x, int arr_in[], int arr_out[], int & r_size) {
     int prod;
-    int v;
     int n_carry=0;
     int carry=0;
 
@@ -13,18 +12,11 @@ void times_engine(int x, int arr_in[], int arr_out[], int & r_size) {
     }
 
     for (int i = 0; i <= r_size; i++) {
-        prod = arr_out[i] * x;
+        prod = (arr_out[i] * x) + carry;
         
         n_carry = (prod-prod%10)/10;
 
-        v = prod%10 + carry;
-        
-        if (v > 9) {
-            n_carry = (v-v%10)/10 + n_carry;
-
-            v = v%10;
-        }
-        arr_out[i] = v;
+        arr_out[i] = prod%10;
 
         carry = n_carry;
     }
@@ -35,19 +27,55 @@ void times_engine(int x, int arr_in[], int arr_out[], int & r_size) {
     }
 }
 
-int main (void) {
-    int arr[500] = {5, 1, 7};
-    int a_size = 2;
-    int arr_p[500] = {5, 9};
-    int p_size = 1;
-    int arr_store[500]={};
-    int arr_res[500]={};
-    int r_size;
-    int s_size;
-    int pow_num;
-    int v;
+void sum_engine(int arr_s[], int arr_r[], int & s_size) {
     int carry;
     int sum;
+    carry = 0;
+    for(int i = 0; i <= s_size; i++) {
+        sum = arr_s[i] + arr_r[i] + carry;
+        carry = (sum-sum%10)/10;
+        arr_s[i] = sum%10;
+    }
+
+    if (carry > 0) {
+        s_size++;
+        arr_s[s_size] = carry;
+    }
+}
+
+void print_array(int arr_in[], int a_size) {
+    for(int i = 0; i <= a_size; i++) {
+        cout << arr_in[a_size-i];
+    }
+}
+
+void large_num_pow(unsigned long long int first_num, unsigned long long int second_num) {
+    int arr[500] = {};
+    int a_size;
+    int arr_p[500] = {};
+    int p_size;
+    int arr_res[500]={};
+    int r_size;
+    int arr_store[500]={};
+    int s_size;
+    int pow_num;
+    cout << first_num << "x" << second_num << "=";
+
+    a_size = 0;
+    while(first_num != 0) {
+        arr[a_size] = first_num%10;
+        first_num = first_num/10;
+        a_size+=1;
+    }
+    a_size=a_size-1;
+
+    p_size = 0;
+    while(second_num != 0) {
+        arr_p[p_size] = second_num%10;
+        second_num = second_num/10;
+        p_size+=1;
+    }
+    p_size=p_size-1;
 
     for(int j=0; j <= p_size; j++) {
         pow_num = arr_p[j];
@@ -77,38 +105,15 @@ int main (void) {
         //makes sure that the store size will be big enough
         //for the sum
         s_size = r_size;
-
-        //add res to store
-        carry = 0;
-        for(int i = 0; i <= s_size; i++) {
-            sum = arr_store[i] + arr_res[i] + carry;
-            v = sum;
-            carry = (v-v%10)/10;
-            if (v > 9) {
-                v = v%10;
-            }
-            arr_store[i] = v;
-        }
-
-        if (carry > 0) {
-            s_size++;
-            arr_store[s_size] = carry;
-        }
+        
+        sum_engine(arr_store, arr_res, s_size);
+        
     }
-
-    for(int i = 0; i <= a_size; i++) {
-        cout << arr[a_size-i];
-    }
-    cout << "x";
-
-    for(int i = 0; i <= p_size; i++) {
-        cout << arr_p[p_size-i];
-    }
-    cout << "=";
-
-    for(int i = 0; i <= s_size; i++) {
-        cout << arr_store[s_size-i];
-    }
+    print_array(arr_store, s_size);
     cout << endl;
+}
+
+int main (void) {
+    large_num_pow(72572875544652895, 655765687512789543);
 
 }
